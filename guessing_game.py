bd5c3e588a58f188419f093d20c930cf69091df3
jjar7266 <jjar7266@gmail.com>
2025-12-12 05:19:03 -0500

    added a .gitignore file. started first project with codemy.com

---
+++ guessing_game.py
@@ -0,0 +1,14 @@
+# Codemy.com: Instructor John Elder
+
+# Import modules
+import os # using to clear the screen
+import random
+
+# Clear the screen
+os.system("cls")  # windows powershell clear command
+
+# Generate a random number and assign it to a variable
+number_to_guess = random.randint(1, 10)
+
+print()
+

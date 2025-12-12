6996dbfe9da6f6ccb9ee0a162662e08b56a0b835
jjar7266 <jjar7266@gmail.com>
2025-12-12 06:31:31 -0500

    modified guessing_game.py made it more pythonic and readable

--- guessing_game.py
+++ guessing_game.py
@@ -1,26 +1,52 @@
 # Codemy.com: Instructor John Elder
+# guessing game
 
 # Import modules
 import os # using to clear the screen
 import random
 
-# Clear the screen
-os.system("cls")  # windows powershell clear command
+# Create our main game function
+def game():
+        
+        # Create a variable to keep track the number of guesses
+        number_of_guesses = 0
+        correct = False
+        
+        # Clear the screen
+        os.system("cls")  # windows powershell clear command
 
-# Generate a random number and assign it to a variable
-number_to_guess = random.randint(1, 10)
+        # Generate a random number and assign it to a variable
+        number_to_guess = random.randint(1, 10)
 
-# Get user input
-print("Guess a number between 1 - 10")
+        # Get user input
+        print("Guess a number between 1 - 10")
 
-# try/except block
-try:
-    guess = int(input("Enter your Guess: "))
-    print(f"You guessed: {guess}")
-except Exception as e:
-    print("Something went wrong")
-    
+        # Create guessing loop
+        while not correct:  # Pythonic: Modified instructors line of code
 
+            # try/except block
+            try:
+                guess = int(input("Enter your Guess: "))
+                print(f"You guessed: {guess}")
+            except ValueError:
+                print("Invalid input - please enter a number.")
+                continue  # Let them try again without ending the game
 
+            number_of_guesses += 1  # Increment once per guess
 
+            # Create some logic to check the guess
+            if guess < number_to_guess:
+                 print("Too Low! - Try Again!")
 
+            elif guess > number_to_guess:
+                 print("Too High! - Try Again!")
+
+            else:
+                print(f"Correct! the number was {number_to_guess}.")
+                print(f"you guessed it in {number_of_guesses} guesses!")
+                # Set correct to TRUE
+                correct = True
+                
+
+# call our game function
+game()
